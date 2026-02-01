37d41f3030c7b6843ff0ebd67c9c881563ea123e
Aaron Matarrita Portuguez <aamatarritaportuguez@gmail.com>
2026-01-31 21:20:45 -0600

    Initialize kanux-mobile React Native app
    Set up the kanux-mobile project with Expo, React Native, navigation structure, screens, assets, configuration files, and TypeScript support. Includes .gitignore files for both root and mobile app, and adds initial package dependencies.

---
+++ kanux-mobile/src/app/AppNavigator.tsx
@@ -0,0 +1,13 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { AuthNavigator } from "@/app/AuthNavigator";
+import { MainNavigator } from "@/app/MainNavigator";
+
+const IS_AUTHENTICATED = true;
+
+export function AppNavigator() {
+  return (
+    <NavigationContainer>
+      {IS_AUTHENTICATED ? <MainNavigator /> : <AuthNavigator />}
+    </NavigationContainer>
+  );
+}
